3af3b4e605d3b5f52a2cbe1a81fe2b5143d6851b
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-07-12 14:40:48 +0300

    Removed all OLD code from JS
    Cleaned before starting solution with Array

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,40 +1,33 @@
 "use strict";
 console.log("script.js file is connected.");
 
-const tableKey = 'cma-table';
-
-let cmaTable;
-
-let cmaTableDemo = {
-    '0': {
-        'firstName': 'Johnny',
-        'lastName': 'Aplleseed',
-        'dateOfBirth': '1845-03-18',
-        'phone': '+37069085856',
-        'email': 'j.aplleseed@icloud.com',
-        'address': 'Cupertino, California, United States'
+let cmaTableArrayDemo = [
+    {   firstName: 'Johnny',
+        lastName: 'Aplleseed',
+        dateOfBirth: '1845-03-18',
+        phone: '+37069085856',
+        email: 'j.aplleseed@icloud.com',
+        address: 'Cupertino, California, United States'
         },
-    '1': {
-        'firstName': 'Sarah',
-        'lastName': 'Connor',
-        'dateOfBirth': '1964-02-13',
-        'phone': '+37060080805',
-        'email': 's.connor@sky.net',
-        'address': '14239 Judgment Avenue, Stars Hollow'
+    {
+        firstName: 'Sarah',
+        lastName: 'Connor',
+        dateOfBirth: '1964-02-13',
+        phone: '+37060080805',
+        email: 's.connor@sky.net',
+        address: '14239 Judgment Avenue, Stars Hollow'
         }
-};
+];
 
-//** Function to Refresh table content:
-let refreshDOMTable = () => {
+console.log("cmaTableArrayDemo Below:");
+console.log(cmaTableArrayDemo);
 
-    let cmaTableKeys = Object.keys(cmaTable);
-    let tableContainer = document.getElementById('cmaTableContainer');
-    let oldTableBody = document.getElementById('tableBody');
-    tableContainer.removeChild(oldTableBody);
 
-    let newTableBody = document.createElement('span');
-    newTableBody.id = 'tableBody';
-    tableContainer.appendChild(newTableBody);
+
+/*
+
+function Exmaple() {
+}
 
     // **** Create Array from Table content:
         console.log("cmaTable Below:");
@@ -108,145 +101,4 @@ let refreshDOMTable = () => {
         newTableBody.appendChild(currentRow);
     }
 
-    //* Function to Enable or Disable "Add New or Edit Contact" Modal:
-        let enableDisableNewUserModal = (option) => {
-            // get data from fields in Modal: 
-            let newContactIdNum = document.getElementById('newContactIdNum');
-            let newContactFirstName = document.getElementById('newContactFirstName');
-            let newContactLastName = document.getElementById('newContactLastName');
-            let newContactDateOfBirth = document.getElementById('newContactDateOfBirth');
-            let newContactPhone = document.getElementById('newContactPhone');
-            let newContactEmail = document.getElementById('newContactEmail');
-            let newContactAddress = document.getElementById('newContactAddress');
-
-            // Reset value in Modal input fields:
-            newContactIdNum.value = "";
-            newContactFirstName.value = "";
-            newContactLastName.value = "";
-            newContactDateOfBirth.value = "";
-            newContactPhone.value = "";
-            newContactEmail.value = "";
-            newContactAddress.value = "";
-
-            // Enable/Disable Modal: 
-            let newContactModal = document.getElementById('newContactModal');
-            newContactModal.className = `${option}-modal`;
-            
-            // Enable/Disable Backdrop:
-            let backdrop = document.getElementById('backdrop');
-            backdrop.className = `${option}-modal`;
-        }
-    //- Function to Enable or Disable "Add New or Edit Contact" Modal.
-
-    
-    //* New Contact Submit Button:
-        let newContactSubmitBtn = document.getElementById('newContactSubmitBtn');
-        newContactSubmitBtn.addEventListener('click', () => {
-            
-            // Trim spaces in values (in front and back of it):
-            let newContactIdNum = document.getElementById('newContactIdNum').value.trim();
-            let newContactFirstName = document.getElementById('newContactFirstName').value.trim();
-            let newContactLastName = document.getElementById('newContactLastName').value.trim();
-            let newContactDateOfBirth = document.getElementById('newContactDateOfBirth').value.trim();
-            let newContactPhone = document.getElementById('newContactPhone').value.trim();
-            let newContactEmail = document.getElementById('newContactEmail').value.trim();
-            let newContactAddress = document.getElementById('newContactAddress').value.trim();
-
-            // Insert VALIDATION HERE:
-            // VALIDATION...
-
-            // Save new values to row collumns if esential fields are not empty:
-            if(newContactFirstName !== "" && newContactPhone !== "" && newContactAddress !== "") {
-                cmaTable[newContactIdNum] = {
-                    'firstName': newContactFirstName,
-                    'lastName': newContactLastName,
-                    'dateOfBirth': newContactDateOfBirth,
-                    'phone': newContactPhone,
-                    'email': newContactEmail,
-                    'address': newContactAddress
-                }
-                localStorage.setItem(tableKey, JSON.stringify(cmaTable));
-                enableDisableNewUserModal('disable');
-                refreshDOMTable();
-            }
-        });
-    //- New Contact Submit Button.
-
-    // New Contact Cancel Button:
-    let newContactCancelBtn = document.getElementById('newContactCancelBtn');
-    newContactCancelBtn.addEventListener('click', () => {
-        enableDisableNewUserModal('disable');
-    });
-
-    // Add Entry Button:
-    let addNewEntryBtn = document.getElementById('cmaAddNewEntry');
-    addNewEntryBtn.addEventListener('click', () => {
-        enableDisableNewUserModal('enable');
-    });
-    
-    //* Edit Contact Button:
-        let editBtns = document.getElementsByClassName('cma-edit');
-        for (let i  = 0; i < editBtns.length; i++) {
-            editBtns[i].addEventListener('click', ($event) => {
-                let rowToEdit = $event.target.parentElement.children[0].innerText;
-                let ContactToEdit = cmaTable[rowToEdit];
-                enableDisableNewUserModal('enable');
-                
-                // Define variables for getting values:
-                let newContactIdNum = document.getElementById('newContactIdNum');
-                let newContactFirstName = document.getElementById('newContactFirstName');
-                let newContactLastName = document.getElementById('newContactLastName');
-                let newContactDateOfBirth = document.getElementById('newContactDateOfBirth');
-                let newContactPhone = document.getElementById('newContactPhone');
-                let newContactEmail = document.getElementById('newContactEmail');
-                let newContactAddress = document.getElementById('newContactAddress');
-
-                // Get values from Modal input fields:
-                newContactIdNum.value = rowToEdit;
-                newContactFirstName.value = ContactToEdit.firstName;
-                newContactLastName.value = ContactToEdit.lastName;
-                newContactDateOfBirth.value = ContactToEdit.dateOfBirth;
-                newContactPhone.value = ContactToEdit.phone;
-                newContactEmail.value = ContactToEdit.email;
-                newContactAddress.value = ContactToEdit.address;
-            });
-        }
-    //- Edit Contact Button.
-    
-    // Delete Button:
-    let deleteBtns = document.getElementsByClassName('cma-delete');
-    for (let i  = 0; i < deleteBtns.length; i++) {
-        deleteBtns[i].addEventListener('click', ($event) => {
-            let contactToDelete = $event.target.parentElement.children[0].innerText;
-            let isSure = window.confirm('Are you sure you want to delete contact from row nr. ' + contactToDelete + '?');
-            if (isSure)
-                deleteRawFromTable(contactToDelete);
-        });
-    }
-}//-- Function to Refresh table content.
-
-let deleteRawFromTable = (rowNumber) => {
-    let tempTable = {};
-    let cmaTableKeys = Object.keys(cmaTable);
-    for (let i = 0; i < cmaTableKeys.length; i++) {
-        if (rowNumber !== cmaTableKeys[i]) {
-            tempTable[cmaTableKeys[i]] = cmaTable[cmaTableKeys[i]];
-        }
-    }
-    cmaTable = tempTable;
-    localStorage.setItem(tableKey, JSON.stringify(cmaTable));
-    refreshDOMTable();
-}
-
-let init = () => {
-    
-    if(localStorage.getItem(tableKey)) {
-        cmaTable = JSON.parse(localStorage.getItem(tableKey));
-    } else {
-        cmaTable = cmaTableDemo;
-    }
-    refreshDOMTable();
-    localStorage.setItem(tableKey, JSON.stringify(cmaTable));
-}
-
-init();
+    */
